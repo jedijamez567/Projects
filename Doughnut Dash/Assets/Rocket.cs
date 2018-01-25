b151d3e6861658a908ced4f58da1ce7dee3b635a
jedijamez567 <smith.dakota52@gmail.com>
2018-01-25 10:49:55 -0600

    Added new ship prefab, made 2 scripts (one for each ship). Ships can now move around.

---
+++ Doughnut Dash/Assets/Rocket.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    Vector3 targetAngle = new Vector3(0f, 180f, 0f);
+    private Vector3 currentAngle;
+    private float lerpTime = 5f;
+    float currentLerpTime = 0f;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        currentAngle = transform.eulerAngles;
+    }
+	
+	// FixedUpdate is called once per frame, used for online multiplayer
+	void FixedUpdate () {
+        ProcessInput();
+        RotateRocket();
+        currentLerpTime += Time.deltaTime;
+	}
+
+    private void RotateRocket()
+    {
+        currentAngle = new Vector3(
+            Mathf.LerpAngle(currentAngle.x, targetAngle.x, currentLerpTime / lerpTime),
+            Mathf.LerpAngle(currentAngle.y, targetAngle.y, currentLerpTime / lerpTime),
+            Mathf.LerpAngle(currentAngle.z, targetAngle.z, currentLerpTime / lerpTime));
+
+        transform.eulerAngles = currentAngle;
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidBody.AddRelativeForce(Vector3.right);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rigidBody.AddRelativeForce(Vector3.left);
+        }
+    }
+}
---
+++ Doughnut Dash/Assets/Rocket2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket2 : MonoBehaviour {
+
+    Rigidbody rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    private void ProcessInput()
+    {
+        // Upwards Thrust (reletave to top of Rocket)
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+
+        // Moves and Rotates Rocket forwards (based on reletave location)
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        // Moves and Rotates Rocket backwards (based on reletave location)
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.back);
+        }
+    }
+}
