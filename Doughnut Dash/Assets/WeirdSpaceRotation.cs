c8e3d78ff30ab03c21630c29521ad75234685878
jedijamez567 <smith.dakota52@gmail.com>
2018-01-27 14:37:09 -0600

    made level 2 awesome!

---
+++ Doughnut Dash/Assets/WeirdSpaceRotation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeirdSpaceRotation : MonoBehaviour {
+
+    static GameObject thisObject;
+    float rotationSpeed = 50f;
+
+	// Use this for initialization
+	void Start () {
+        thisObject = gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rotate();
+	}
+
+    private void Rotate()
+    {
+        thisObject.transform.Rotate(Vector3.left, rotationSpeed * Time.deltaTime);
+    }
+}
